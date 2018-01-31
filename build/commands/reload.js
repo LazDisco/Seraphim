ffe4d742cc34ac0ca339d351c9b351e501132434
unknown <lazdisco@gmail.com>
2018-01-31 17:58:05 +0000

    Added ability to manually restart the lists.

---
+++ build/commands/reload.js
@@ -0,0 +1,12 @@
+import { playerlist } from "./processes/playerlist.js";
+import { watchlist } from "./processes/watchlist.js";
+
+module.exports.run = async (msg, args, client, db, ID) => {
+    playerlist()
+    watchlist()
+    msg.channel.send("API lists and connection has been refreshed.")
+}
+
+module.exports.help = {
+    name: "reload"
+}

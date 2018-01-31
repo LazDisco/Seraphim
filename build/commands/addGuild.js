d5b2dec3dbff2df4b852cd9dd23663d85a60cd46
unknown <lazdisco@gmail.com>
2018-01-31 18:33:44 +0000

    fixed guildadd

---
+++ build/commands/addGuild.js
@@ -0,0 +1,15 @@
+import secrets from '../secrets.json'
+
+module.exports.run = async (msg, args, client, db, ID) => {
+    if (msg.author.id !== secrets.ownerID) return msg.reply("ERR: This command is locked to bot owner only.")
+
+    let id = args[0]
+
+    db.createGuild(id)
+        .then(msg.channel.send(`Info: Successfully created row \`${id}\`.`))
+        .catch(msg.channel.send("Warn: Table already exists."))
+}
+
+module.exports.help = {
+    name: "addguild"
+}

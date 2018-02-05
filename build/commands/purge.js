eb737540587e90acb331ccfa1bb617e0b71607b0
unknown <lazdisco@gmail.com>
2018-02-05 14:33:15 +0000

    Changed purge to include authors command message no matter what number. (|purge 1 will remove one message + the command message)

--- build/commands/purge.js
+++ build/commands/purge.js
@@ -1,21 +1,18 @@
 module.exports.run = async (msg, args, client, db, ID) => {
-    let sender = msg.member;
-    if (!sender.hasPermission('MANAGE_MESSAGES')) return msg.reply("ERR: User doesn't have valid permissions to use this command")
-    // Make sure that they have the right permissions before using the command.
-
-    // Also I was really tired so I just c+p'ed this code from "an idiot's guide". Kudos.
-    const user = msg.mentions.users.first(); // If a user is mentioned.
-    const amount = !!parseInt(msg.content.split(' ')[1]) ? parseInt(msg.content.split(' ')[1]) : parseInt(msg.content.split(' ')[2])
+    let user = msg.mentions.users.first();
+    let amount = !!parseInt(msg.content.split(' ')[1]) ? parseInt(msg.content.split(' ')[1]) : parseInt(msg.content.split(' ')[2])
+    amount = amount+1
     if (!amount) return msg.reply('Must specify an amount to delete!');
-    if (!amount && !user) return msg.reply('Must specify a user and amount, or just an amount, of msgs to purge!');
-    msg.channel.fetchmsgs({
+    if (!amount && !user) return msg.reply('Must specify a user and amount, or just an amount, of messages to purge!');
+    
+    msg.channel.fetchMessages({
         limit: amount,
-    }).then((msgs) => {
+    }).then((messages) => {
         if (user) {
             const filterBy = user ? user.id : Client.user.id;
-            msgs = msgs.filter(m => m.author.id === filterBy).array().slice(0, amount);
+            messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
         }
-        msg.channel.bulkDelete(msgs, true).catch(error => console.log(error.stack));
+        msg.channel.bulkDelete(messages).catch(error => console.log(error.stack));
     });
 }
 

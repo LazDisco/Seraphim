53854e7fe93884e867ca14614956779d1ca4ee7b
Laz <lazdisco@gmail.com>
2018-03-30 11:13:57 +0100

    Change to setPresence

--- build/events/ready.js
+++ build/events/ready.js
@@ -5,11 +5,9 @@ import { watchlist } from "../processes/watchlist.js";
 module.exports = (client) => {
     winston.info("Bot is ready.\n") // New line to show when things are finally 100% ready.
     client.user.setPresence({
-        status: 'idle', // Yellow?
-        afk: true, // I have no idea what this does, I just want to see.
         game: {
-            name: "Remy's screams!", // I mean, how could I not make this joke?
-            type: "LISTENING" // Poor Remy.
+            name: "the Discovery Angels",
+            type: "LISTENING"
         }
     })
     playerlist()
